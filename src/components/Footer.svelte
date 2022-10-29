c1e6ad616148fc7603761279aece81cb4088236c
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-10-29 15:50:01 +0200

    bugfix tailwindcss footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <div class="w-full p-2 text-center mt-10 bg-slate-100">
 	<p>
 		Made with SvelteKit (<a
-			class="text-underline"
+			class="underline"
 			href="https://github.com/mandrasch/landingpage-matthiasandrasch/tree/sveltekit-conversion"
 			>Source</a
 		>) | Emojis by <a href="https://openmoji.org/" class="underline">openmoji.org</a> (CC BY ShareAlike
