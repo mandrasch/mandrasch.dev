1cb8fef816f7ed7c6fcd4eece4e9145831856fb7
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-10-29 13:51:27 +0200

    switch to adapter-node for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+// https://kit.svelte.dev/docs/adapters#supported-environments-node-js
+import adapter from '@sveltejs/adapter-node';
 import preprocess from "svelte-preprocess";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter()
+		/*
+		// these were for static
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
@@ -13,7 +19,7 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		})*/
 	},
 	preprocess: [
 		preprocess({
