dd9e34e1a4b7e195682a4616f17cf34bc0aaa3f3
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-11-06 11:04:31 +0100

    change footer link to repo

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,8 +2,7 @@
 	<p>
 		Made with SvelteKit (<a
 			class="underline"
-			href="https://github.com/mandrasch/landingpage-matthiasandrasch/tree/sveltekit-conversion"
-			>Source</a
+			href="https://github.com/mandrasch/landingpage-matthiasandrasch">Source</a
 		>) |
 		<a href="https://matthias-andrasch.eu/blog/impressum-datenschutz/" class="underline"
 			>Imprint & Privacy (Datenschutz &amp; Impressum)</a
