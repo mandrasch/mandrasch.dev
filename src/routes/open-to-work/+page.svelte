ad38ddffb2e4c26c465b8296bfadc80d4eee2c86
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-12-26 10:20:12 +0100

    feat: open to work page, first draft

---
+++ src/routes/open-to-work/+page.svelte
@@ -0,0 +1,56 @@
+<div class="container mx-auto prose mb-10">
+	<div class="flex flex-col space-y-2">
+		<h2>Neue Job-Heimat gesucht</h2>
+
+		<h3>Profil</h3>
+
+		<ul>
+			<li>Lieblings-Stack: Svelte(Kit), HTML / SCSS / JS, PHP (DDEV)</li>
+
+			<li>
+				"Junior Developer" - hauptberuflicher Wieder-Einstieg als Web-Programmierer im September
+				2020, sehr motiviert weiter zu lernen und mich weiterzuentwickeln.
+			</li>
+			<li>
+				Allrounder, interessiert an Themen Frontend, Backend, UX/UI sowie teils auch Dev-Ops-Themen.
+				Derzeit auf der Suche nach Arbeitsfeldern, in denen ich mich weiter spezialisieren und
+				Erfahrungen sammeln kann.
+			</li>
+		</ul>
+
+		<h3>Erfahrung</h3>
+
+		<ul>
+			<li>
+				Web-Agentur gugler* MarkenSinn, St. Pölten
+				<ul>
+					<li>Frontend-Entwicklung mit Bootstrap, Eleventy, Vanilla-JavaScript, Svelte</li>
+					<li>PHP-Entwicklung mit WordPress + DDEV</li>
+					<li>teilweise Mitarbeit in typo3- und Laravel-Projekten</li>
+					<li>PWA-Entwicklung mit Ionic + Angular</li>
+					<li>
+						Angefangen, mich in in typo3, Barrierefreies Web-Design, Sustainable Web Design
+						einzulernen
+					</li>
+				</ul>
+			</li>
+			<li>
+				Privat / Open Source
+				<ul>
+					<li>Einlernen in SvelteKit, TailwindCSS, Pocketbase, etc.</li>
+					<li>Arbeit an DDEV Workflows für WordPress, vite, Laravel</li>
+					<li>Ausprobieren von CMS-Lösungen wie CraftCMS, Statamic, KirbyCMS, etc.</li>
+				</ul>
+			</li>
+			<li>Sonstige Skills: Bloggen</li>
+		</ul>
+
+		<h3>Wunsch-Liste</h3>
+		<ul>
+			<li>Gemeinsame Arbeit im Team, nicht ausschließlich Einzelkämpfertum</li>
+			<li>Flexible Arbeitszeit-Einteilung, ca. 35h/Woche, Home-Office-Option</li>
+			<li>Höhenverstellbarer Schreibtisch</li>
+			<li>Öffentlich gut erreichbare Büro-Location (Bin Klimaticket-Besitzer)</li>
+		</ul>
+	</div>
+</div>
